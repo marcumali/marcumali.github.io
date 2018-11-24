a000ad9be4c558f9d253a3e35a68167fc3fb1290
Marc Umali <janmarcquianoumali@gmail.com>
2018-11-24 20:43:03 +0800

    defer offscreen images ..

---
+++ assets/scripts/defer.js
@@ -0,0 +1,18 @@
+var deferFnc = {};
+
+( function( $ ){
+
+    deferFnc.Img = function(){
+
+        var imgDefer = document.getElementsByTagName( 'img' );
+        for ( var i = 0; i < imgDefer.length; i++ ) {
+            if ( imgDefer[i].getAttribute('data-src') ) {
+                imgDefer[i].setAttribute( 'src',imgDefer[i].getAttribute('data-src') );
+            }
+        }
+
+    };
+
+    deferFnc.Img();
+
+}) (jQuery);

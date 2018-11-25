db2dfff27af8a9c1ac72509d864e390082acb6e3
Marc Umali <janmarcquianoumali@gmail.com>
2018-11-25 20:49:50 +0800

    defer background images ...

--- assets/scripts/defer.js
+++ assets/scripts/defer.js
@@ -4,12 +4,26 @@ var deferFnc = {};
 
     deferFnc.Img = function(){
 
-        var imgDefer = document.getElementsByTagName( 'img' );
-        for ( var i = 0; i < imgDefer.length; i++ ) {
-            if ( imgDefer[i].getAttribute('data-src') ) {
-                imgDefer[i].setAttribute( 'src',imgDefer[i].getAttribute('data-src') );
-            }
-        }
+        var imgDefer = $( 'img' );
+        var imgBgDefer = $( '.js-defer-bg' );
+
+        $.each( imgDefer, function( index, value ){
+
+            var _this = $( value );
+            var dataSrc = _this.data( 'src' );
+
+            _this.attr( 'src', dataSrc );
+
+        });
+
+        $.each( imgBgDefer, function( index, value ){
+
+            var _this = $( value );
+            var dataSrc = _this.data( 'src' );
+
+            _this.attr( 'style', 'background-image: url( ' + dataSrc + ')' );
+
+        });
 
     };
 

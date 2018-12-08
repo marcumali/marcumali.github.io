e8b8c73ef0dc627d05d9261aeb7a22ede5ad6227
Marc Umali <janmarcquianoumali@gmail.com>
2018-12-08 15:38:28 +0800

    update sw ..

--- pwa/app.js
+++ pwa/app.js
@@ -1,5 +1,5 @@
 if ( 'serviceWorker' in navigator ) {
-    navigator.serviceWorker.register( '/pwa/service-worker.js' )
+    navigator.serviceWorker.register( '/sw.js' )
     .then( function(){
         console.log( 'SW Registered' );
     });

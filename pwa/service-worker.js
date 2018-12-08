583ca3f41a968ed5cf8d6791e58b45203f98ef05
Marc Umali <janmarcquianoumali@gmail.com>
2018-12-08 15:23:03 +0800

    update sw ..

--- pwa/service-worker.js
+++ pwa/service-worker.js
@@ -1,18 +1,31 @@
-self.addEventListener( 'install', function( event ){
-    console.log( 'SW installed' );
-    event.waitUntil(
-        caches.open( 'static' )
-        .then( function( cache ){
-            cache.addAll([
-                '/',
-                '/index.html',
-                '/dist/styles/main.css',
-                '/dist/styles/main-critical.css'
-            ]);
-        });
-    );
+self.addEventListener('install', function (event) {
+  console.log('SW Installed');
+  event.waitUntil(
+    caches.open('static')
+      .then(function (cache) {
+        cache.addAll([
+          '/',
+          '/index.html',
+          '/dist/styles/main.css',
+          '/dist/styles/main-critical.css'
+        ]);
+      })
+  );
 });
 
-self.addEventListener( 'activate', function(){
-    console.log( 'SW Activated' );
+self.addEventListener('activate', function () {
+  console.log('SW Activated');
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(res) {
+        if (res) {
+          return res;
+        } else {
+          return fetch(event.request);
+        }
+      })
+  );
 });
